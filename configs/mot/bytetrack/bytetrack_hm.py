1b66082280bcd2aa118252f36295d4bd40a2783b
Christopher Olivier <cjolivier01@gmail.com>
2024-03-03 08:44:40 -0800

    seems tracking reasonably well

--- configs/mot/bytetrack/bytetrack_hm.py
+++ configs/mot/bytetrack/bytetrack_hm.py
@@ -1,4 +1,9 @@
-detection_img_scale = (1333, 800)
+
+# Stitched panoramic video aspect ratio is about 2.4
+#detection_img_scale = (1333, 800)
+#detection_img_scale = (2600, 1600)
+#detection_img_scale = (1920, 800)
+detection_img_scale = (1680, 700)
 
 dcn_detector = dict(
     type="FasterRCNN",
@@ -124,185 +129,25 @@ dcn_detector = dict(
     ),
 )
 
-# orig_detector = dict(
-#     type="FasterRCNN",
-#     backbone=dict(
-#         type="ResNet",
-#         depth=50,
-#         num_stages=4,
-#         out_indices=(0, 1, 2, 3),
-#         frozen_stages=1,
-#         norm_cfg=dict(type="BN", requires_grad=True),
-#         norm_eval=True,
-#         style="pytorch",
-#         init_cfg=dict(type="Pretrained", checkpoint="torchvision://resnet50"),
-#     ),
-#     neck=dict(
-#         type="FPN", in_channels=[256, 512, 1024, 2048], out_channels=256, num_outs=5
-#     ),
-#     rpn_head=dict(
-#         type="RPNHead",
-#         in_channels=256,
-#         feat_channels=256,
-#         anchor_generator=dict(
-#             type="AnchorGenerator",
-#             scales=[8],
-#             ratios=[0.5, 1.0, 2.0],
-#             strides=[4, 8, 16, 32, 64],
-#         ),
-#         bbox_coder=dict(
-#             type="DeltaXYWHBBoxCoder",
-#             target_means=[0.0, 0.0, 0.0, 0.0],
-#             target_stds=[1.0, 1.0, 1.0, 1.0],
-#             clip_border=False,
-#         ),
-#         loss_cls=dict(type="CrossEntropyLoss", use_sigmoid=True, loss_weight=1.0),
-#         loss_bbox=dict(type="SmoothL1Loss", beta=0.1111111111111111, loss_weight=1.0),
-#     ),
-#     roi_head=dict(
-#         type="StandardRoIHead",
-#         bbox_roi_extractor=dict(
-#             type="SingleRoIExtractor",
-#             roi_layer=dict(type="RoIAlign", output_size=7, sampling_ratio=0),
-#             out_channels=256,
-#             featmap_strides=[4, 8, 16, 32],
-#         ),
-#         bbox_head=dict(
-#             type="Shared2FCBBoxHead",
-#             in_channels=256,
-#             fc_out_channels=1024,
-#             roi_feat_size=7,
-#             num_classes=1,
-#             bbox_coder=dict(
-#                 type="DeltaXYWHBBoxCoder",
-#                 target_means=[0.0, 0.0, 0.0, 0.0],
-#                 target_stds=[0.1, 0.1, 0.2, 0.2],
-#                 clip_border=False,
-#             ),
-#             reg_class_agnostic=False,
-#             loss_cls=dict(type="CrossEntropyLoss", use_sigmoid=False, loss_weight=1.0),
-#             loss_bbox=dict(type="SmoothL1Loss", loss_weight=1.0),
-#         ),
-#     ),
-#     train_cfg=dict(
-#         rpn=dict(
-#             assigner=dict(
-#                 type="MaxIoUAssigner",
-#                 pos_iou_thr=0.7,
-#                 neg_iou_thr=0.3,
-#                 min_pos_iou=0.3,
-#                 match_low_quality=True,
-#                 ignore_iof_thr=-1,
-#             ),
-#             sampler=dict(
-#                 type="RandomSampler",
-#                 num=256,
-#                 pos_fraction=0.5,
-#                 neg_pos_ub=-1,
-#                 add_gt_as_proposals=False,
-#             ),
-#             allowed_border=-1,
-#             pos_weight=-1,
-#             debug=False,
-#         ),
-#         rpn_proposal=dict(
-#             nms_pre=2000,
-#             max_per_img=1000,
-#             nms=dict(type="nms", iou_threshold=0.7),
-#             min_bbox_size=0,
-#         ),
-#         rcnn=dict(
-#             assigner=dict(
-#                 type="MaxIoUAssigner",
-#                 pos_iou_thr=0.5,
-#                 neg_iou_thr=0.5,
-#                 min_pos_iou=0.5,
-#                 match_low_quality=False,
-#                 ignore_iof_thr=-1,
-#             ),
-#             sampler=dict(
-#                 type="RandomSampler",
-#                 num=512,
-#                 pos_fraction=0.25,
-#                 neg_pos_ub=-1,
-#                 add_gt_as_proposals=True,
-#             ),
-#             pos_weight=-1,
-#             debug=False,
-#         ),
-#     ),
-#     test_cfg=dict(
-#         rpn=dict(
-#             nms_pre=1000,
-#             max_per_img=1000,
-#             nms=dict(type="nms", iou_threshold=0.7),
-#             min_bbox_size=0,
-#         ),
-#         rcnn=dict(
-#             score_thr=0.05, nms=dict(type="nms", iou_threshold=0.5), max_per_img=100
-#         ),
-#     ),
-#     init_cfg=dict(
-#         type="Pretrained",
-#         checkpoint="https://download.openmmlab.com/mmtracking/mot/faster_rcnn/faster-rcnn_r50_fpn_4e_mot17-ffa52ae7.pth",
-#     ),
-# )
-
-
+#
+# The overall tracking model
+#
 model = dict(
     detector=dcn_detector,
-    # detector=orig_detector,
-    type="Tracktor",
-    reid=dict(
-        type="BaseReID",
-        backbone=dict(
-            type="ResNet", depth=50, num_stages=4, out_indices=(3,), style="pytorch"
-        ),
-        neck=dict(type="GlobalAveragePooling", kernel_size=(8, 4), stride=1),
-        head=dict(
-            type="LinearReIDHead",
-            num_fcs=1,
-            in_channels=2048,
-            fc_channels=1024,
-            out_channels=128,
-            num_classes=380,
-            loss=dict(type="CrossEntropyLoss", loss_weight=1.0),
-            loss_pairwise=dict(type="TripletLoss", margin=0.3, loss_weight=1.0),
-            norm_cfg=dict(type="BN1d"),
-            act_cfg=dict(type="ReLU"),
-        ),
-        init_cfg=dict(
-            type="Pretrained",
-            checkpoint="https://download.openmmlab.com/mmtracking/mot/reid/reid_r50_6e_mot17-4bf6b63d.pth",  # noqa: E251  # noqa: E501
-        ),
-    ),
-    motion=dict(
-        type="CameraMotionCompensation",
-        warp_mode="cv2.MOTION_EUCLIDEAN",
-        num_iters=100,
-        stop_eps=0.00001,
-    ),
+    type="ByteTrack",
+    motion=dict(type="KalmanFilter"),
     tracker=dict(
-        type="TracktorTracker",
-        obj_score_thr=0.5,
-        regression=dict(
-            obj_score_thr=0.5,
-            nms=dict(type="nms", iou_threshold=0.6),
-            match_iou_thr=0.3,
-        ),
-        reid=dict(
-            num_samples=10,
-            img_scale=(256, 128),
-            img_norm_cfg=None,
-            match_score_thr=2.0,
-            match_iou_thr=0.2,
-        ),
-        momentums=None,
-        num_frames_retain=10,
+        type="ByteTracker",
+        # obj_score_thrs=dict(high=0.6, low=0.1),
+        # init_track_thr=0.7,
+        obj_score_thrs=dict(high=0.4, low=0.1),
+        init_track_thr=0.3,
+        # obj_score_thrs=dict(high=0.99, low=0.99),
+        # init_track_thr=0.99,
+        weight_iou_with_det_scores=True,
+        match_iou_thrs=dict(high=0.1, low=0.5, tentative=0.3),
+        num_frames_retain=30,
     ),
-    # type="DeepSORT",
-    # motion=dict(type="KalmanFilter", center_only=False),
-    # tracker=dict(type="SortTracker", obj_score_thr=0.5, match_iou_thr=0.5, reid=None),
 )
 
 data = dict(
