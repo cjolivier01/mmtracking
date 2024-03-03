388cfe28e4168f8e61146a895fcd386e0e69e2a2
Christopher Olivier <cjolivier01@gmail.com>
2024-03-03 14:07:37 -0800

    hm bytetrack

--- configs/mot/bytetrack/bytetrack_hm.py
+++ configs/mot/bytetrack/bytetrack_hm.py
@@ -1,34 +1,40 @@
 _base_ = [
-    "../../_base_/datasets/coco_detection.py",
+    "../../../../mmdetection/configs/_base_/datasets/coco_detection.py",
+    '../../../../mmdetection/configs/_base_/schedules/schedule_1x.py', 
+    '../../../../mmdetection/configs/_base_/default_runtime.py'
 ]
 
-checkpoint_config = dict(interval=1)
-# yapf:disable
-log_config = dict(
-    interval=50,
-    hooks=[
-        dict(type='TextLoggerHook'),
-        # dict(type='TensorboardLoggerHook')
-    ])
-# yapf:enable
-custom_hooks = [dict(type='NumClassCheckHook')]
+max_epochs = 24
 
-dist_params = dict(backend='nccl')
-log_level = 'INFO'
-load_from = None
-resume_from = None
-workflow = [('train', 1)]
+runner = dict(type='EpochBasedRunner', max_epochs=max_epochs)
 
-# disable opencv multithreading to avoid system being overloaded
-opencv_num_threads = 0
-# set multi-process start method as `fork` to speed up the training
-mp_start_method = 'fork'
+# checkpoint_config = dict(interval=1)
+# # yapf:disable
+# log_config = dict(
+#     interval=50,
+#     hooks=[
+#         dict(type='TextLoggerHook'),
+#         # dict(type='TensorboardLoggerHook')
+#     ])
+# # yapf:enable
+# custom_hooks = [dict(type='NumClassCheckHook')]
 
-# Default setting for scaling LR automatically
-#   - `enable` means enable scaling LR automatically
-#       or not by default.
-#   - `base_batch_size` = (8 GPUs) x (2 samples per GPU).
-auto_scale_lr = dict(enable=False, base_batch_size=16)
+# dist_params = dict(backend='nccl')
+# log_level = 'INFO'
+# load_from = None
+# resume_from = None
+# workflow = [('train', 1)]
+
+# # disable opencv multithreading to avoid system being overloaded
+# opencv_num_threads = 0
+# # set multi-process start method as `fork` to speed up the training
+# mp_start_method = 'fork'
+
+# # Default setting for scaling LR automatically
+# #   - `enable` means enable scaling LR automatically
+# #       or not by default.
+# #   - `base_batch_size` = (8 GPUs) x (2 samples per GPU).
+# auto_scale_lr = dict(enable=False, base_batch_size=16)
 
 
 
@@ -183,8 +189,8 @@ bytetrack_model = dict(
     ),
 )
 
-#model = dcn_detector
-model = bytetrack_model
+model = dcn_detector
+#model = bytetrack_model
 
 # data = dict(
 #     samples_per_gpu=2,
